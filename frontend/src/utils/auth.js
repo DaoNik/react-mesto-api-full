340c1cd0e933899bb7ec9a9065b4456da5b19b8a
DaoNik <nikitataranin@gmail.com>
2022-02-02 15:46:40 +0300

    fix

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.pro100grog.students.nomoredomains.work/';
+export const BASE_URL = 'https://api.pro100grog.students.nomoredomains.work';
 
 function checkRequest(res) {
   if (res.ok) {
